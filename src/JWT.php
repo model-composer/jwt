0f800575eacced70879224835edcd11f27690345
Domenico Giambra <zorinik@gmail.com>
2022-05-06 16:57:01 +0200

    Using colons as redis separator

--- src/JWT.php
+++ src/JWT.php
@@ -42,7 +42,8 @@ class JWT
 		} else {
 			$cache = Cache::getCacheAdapter($config['type']);
 
-			return $cache->get('model.jwt.key', function (ItemInterface $item) {
+			return $cache->get('model:jwt:key', function (ItemInterface $item) {
+				$item->expiresAfter(3600 * 24 * 365);
 				return bin2hex(random_bytes(64));
 			});
 		}
